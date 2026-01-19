49bd9e6a7e2506a57ad373934a9bc004a1aae531
hiraiushiroda <skyblue.youth725@gmail.com>
2026-01-19 11:10:34 +0900

    配列の練習2

--- study.js
+++ study.js
@@ -1,9 +1,9 @@
 //配列　Array
 //1.[]を使って、三人分の名前を１つの箱に入れる
-const members = ["後田", "田中", "佐藤"]
+const members = ["後田", "田中", "佐藤","ちいかわ","シーサー"];
 
 //2.0番目を取り出す
-console.log(members[0]);
+console.log(members[4]);
 
 //3.全員の人数を数える
 console.log(members.length);
