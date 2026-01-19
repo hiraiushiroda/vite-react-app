5b17fef85d935806d6438a0c23383f0ed809f988
hiraiushiroda <skyblue.youth725@gmail.com>
2026-01-19 11:59:39 +0900

    連想配列の練習

--- study.js
+++ study.js
@@ -18,3 +18,23 @@ const users = [
 //3番目の人の名前を出す
 console.log(users[2].name);
 //=========================================
+//連想配列の練習(1/19完了)
+//データの作成　配列の中に連想配列・プロフィール入れる
+const chiikawaFamily = [
+    { name: "ちいかわ", type:"くま", level:5 },
+    { name: "ハチワレ", type:"ねこ", level:7 },
+    { name: "うさき", tyoe:"うさぎ", level:10 },
+];
+//特定のデータを取り出す
+//ハチワレのtypeを出したい
+console.log(chiikawaFamily[1].type);
+
+//うさぎのレベルをテンプレートリテラルで出す
+console.log(`${chiikawaFamily[2].name}のレベルは${chiikawaFamily[2].level}です`);
+
+//データの追加
+//新しいメンバー(連想配列)を配列の最後に追加
+chiikawaFamily.push({ name: "ラッコ", type: "ラッコ", level: 20 });
+
+//追加されたか確認する
+console.log(chiikawaFamily.length);
